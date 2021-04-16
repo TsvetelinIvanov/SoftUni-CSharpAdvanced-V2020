b4f50ac70dbc6811070e80b39f0789b72b032a66
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-04-16 21:53:35 +0300

    Add 09CSharpAdvancedV2020_IteratorsAndComparatorsLab

---
+++ CSharpAdvancedV2020/09CSharpAdvancedV2020_IteratorsAndComparatorsLab/04BookComparator/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors;
+        }
+
+        public string Title { get; private set; }
+
+        public int Year { get; private set; }
+
+        public IReadOnlyCollection<string> Authors { get; private set; }
+
+        public int CompareTo(Book otherBook)
+        {
+            int comparationResult = this.Year.CompareTo(otherBook.Year);
+            if (comparationResult == 0)
+            {
+                comparationResult = this.Title.CompareTo(otherBook.Title);
+            }
+
+            return comparationResult;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ CSharpAdvancedV2020/09CSharpAdvancedV2020_IteratorsAndComparatorsLab/04BookComparator/BookComparator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book firstBook, Book secondBook)
+        {
+            int comparationResult = firstBook.Title.CompareTo(secondBook.Title);
+            if (comparationResult == 0)
+            {
+                comparationResult = secondBook.Year.CompareTo(firstBook.Year);
+            }
+
+            return comparationResult;
+        }
+    }
+}
