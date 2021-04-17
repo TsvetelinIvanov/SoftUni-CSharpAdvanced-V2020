115c1b24594df39280480facf8b72aad146391f1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-04-17 05:18:12 +0300

    Add 03CSharpAdvancedOOPVI2020_InterfacesAndAbstractionLab

---
+++ CSharpOOPVI2020/03CSharpAdvancedOOPVI2020_InterfacesAndAbstractionLab/01Shapes/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public void Draw()
+        {
+            double innerRadius = this.radius - 0.4;
+            double outerRadius = this.radius + 0.4;
+            for (double i = this.radius; i >= -this.radius; --i)
+            {
+                for (double j = -this.radius; j < outerRadius; j += 0.5)
+                {
+                    double value = j * j + i * i;
+                    if (value >= innerRadius * innerRadius && value <= outerRadius * outerRadius)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
