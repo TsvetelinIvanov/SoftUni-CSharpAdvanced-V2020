4db25a5d613dc7c4db41f8b1581e46748d877904
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-04-18 07:52:43 +0300

    Add 21BehavioralPatterns_Memento

---
+++ CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/22BehavioralPatterns_State/OverdrawnState.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _22BehavioralPatterns_State
+{
+    public class OverdrawnState : State
+    {
+        private double serviceFee;
+
+        public OverdrawnState(State state)
+        {
+            this.Balance = state.Balance;
+            this.Account = state.Account;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.interest = 0.0;
+            this.lowerLimit = -100.0;
+            this.upperLimit = 0.0;
+            this.serviceFee = 15.00;
+        }
+
+        public override void Deposit(double amount)
+        {
+            this.Balance += amount;
+
+            this.StateChangeCheck();
+        }
+
+        public override void Withdraw(double amount)
+        {
+            Console.WriteLine("No funds available for withdrawal!");
+        }
+
+        public override void PayInterest()
+        {
+            // No interest is paid
+        }
+
+        private void StateChangeCheck()
+        {
+            if (this.Balance > upperLimit)
+            {
+                this.Account.State = new StandardState(this);
+            }
+        }
+    }
+}
