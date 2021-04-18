549b2a1bda6b5df0fd8b3a732a378ab2bf14bef6
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-04-18 07:44:17 +0300

    Add 15BehavioralPatterns_ChainOfResponsibility

---
+++ CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/15BehavioralPatterns_ChainOfResponsibility/Approver.cs
@@ -0,0 +1,14 @@
+﻿namespace _15BehavioralPatterns_ChainOfResponsibility
+{
+    public abstract class Approver
+    {
+        protected Approver successor;
+
+        public void SetSuccessor(Approver successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+}
---
+++ CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/15BehavioralPatterns_ChainOfResponsibility/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _15BehavioralPatterns_ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Approver ronny = new Director();
+            Approver bobby = new VicePresident();
+            Approver ricky = new President();
+
+            ronny.SetSuccessor(bobby);
+            bobby.SetSuccessor(ricky);
+
+            Purchase purchase = new Purchase(8884, 350.00, "Assets");
+            ronny.ProcessRequest(purchase);//Director approved request# 8884
+
+            purchase = new Purchase(5675, 33390.10, "Project Poison");
+            ronny.ProcessRequest(purchase);//President approved request# 5675
+
+            purchase = new Purchase(5676, 144400.00, "Project BBD");
+            ronny.ProcessRequest(purchase);//Request# 5676 requires an executive meeting!
+        }
+    }
+}
