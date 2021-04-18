306cb82f510d408b231311f644c1ceff7a4a9915
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-04-18 07:53:31 +0300

    Add 21BehavioralPatterns_Memento

---
+++ CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/23BehavioralPatterns_Interpreter/Context.cs
@@ -0,0 +1,14 @@
+﻿namespace _23BehavioralPatterns_Interpreter
+{
+    public class Context
+    {        
+        public Context(string input)
+        {
+            this.Input = input;
+        }
+
+        public string Input { get; set; }
+
+        public int Output { get; set; }
+    }
+}
