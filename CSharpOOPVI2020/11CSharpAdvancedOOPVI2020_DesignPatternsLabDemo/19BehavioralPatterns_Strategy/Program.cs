2478c3450f8c18fecb74c44fb0bd7fbfa840e6a1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-04-18 07:48:12 +0300

    Add 19BehavioralPatterns_Strategy

---
+++ CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/19BehavioralPatterns_Strategy/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _19BehavioralPatterns_Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList studentRecord = new SortedList();
+            studentRecord.Add("Bobby");           
+            studentRecord.Add("Kate");
+            studentRecord.Add("Mike");
+            studentRecord.Add("Ricky");
+            studentRecord.Add("Ronny");
+
+            studentRecord.SetSortStrategy(new QuickSort());
+            studentRecord.Sort();//QuickSorted list
+                                 //Bobby
+                                 //Kate
+                                 //Mike
+                                 //Ricky
+                                 //Ronny
+
+            studentRecord.SetSortStrategy(new ShellSort());
+            studentRecord.Sort();//ShellSorted list
+                                 //Bobby
+                                 //Kate
+                                 //Mike
+                                 //Ricky
+                                 //Ronny
+
+            studentRecord.SetSortStrategy(new MergeSort());
+            studentRecord.Sort();//MergeSorted list
+                                 //Bobby
+                                 //Kate
+                                 //Mike
+                                 //Ricky
+                                 //Ronny
+        }
+    }
+}
---
+++ CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/19BehavioralPatterns_Strategy/SortedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _19BehavioralPatterns_Strategy
+{
+    public class SortedList
+    {
+        private List<string> list = new List<string>();
+        private SortStrategy sortStrategy;
+
+        public void SetSortStrategy(SortStrategy sortsSrategy)
+        {
+            this.sortStrategy = sortsSrategy;            
+        }
+
+        public void Add(string name)
+        {
+            this.list.Add(name);
+        }
+
+        public void Sort()
+        {
+            sortStrategy.Sort(this.list);
+
+            foreach (string name in this.list)
+            {
+                Console.WriteLine(" " + name);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
