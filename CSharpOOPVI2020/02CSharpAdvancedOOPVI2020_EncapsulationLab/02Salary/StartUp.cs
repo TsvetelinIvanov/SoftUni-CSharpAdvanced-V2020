c4b88399912acb97fa7e8d69d890e1280bdcfb3c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-07-28 04:56:31 +0300

    Update StartUp.cs

--- CSharpOOPVI2020/02CSharpAdvancedOOPVI2020_EncapsulationLab/02Salary/StartUp.cs
+++ CSharpOOPVI2020/02CSharpAdvancedOOPVI2020_EncapsulationLab/02Salary/StartUp.cs
@@ -13,9 +13,9 @@ namespace PersonsInfo
             {
                 string[] commandArgs = Console.ReadLine().Split();
                 Person person = new Person(commandArgs[0], commandArgs[1], int.Parse(commandArgs[2]), decimal.Parse(commandArgs[3]));
-
                 persons.Add(person);
             }
+            
             decimal parcentage = decimal.Parse(Console.ReadLine());
             persons.ForEach(p => p.IncreaseSalary(parcentage));
             persons.ForEach(p => Console.WriteLine(p.ToString()));
