3b387263ea82575026962f7977a920a95abd6131
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-07-31 16:48:22 +0300

    Update AccountManager.cs

--- CSharpOOPVI2020/07CSharpAdvancedOOPVI2020_SOLIDExerciseDemo/08InterfaceSegregation_Identity/AccountManager.cs
+++ CSharpOOPVI2020/07CSharpAdvancedOOPVI2020_SOLIDExerciseDemo/08InterfaceSegregation_Identity/AccountManager.cs
@@ -24,7 +24,7 @@ namespace _08InterfaceSegregation_Identity
             user.ChangePassword(this.MockHashPassword(newPass));
         }
 
-        public string MockHashPassword(string password)//Not for using in real applications, where must be used real hash algorithm
+        public string MockHashPassword(string password)//Not for using in real applications, where a real hash algorithm must be used!
         {
             return password + "!A9m+o6@1#al$%^&8*&^%^$1p0#I^9K*";
         }
