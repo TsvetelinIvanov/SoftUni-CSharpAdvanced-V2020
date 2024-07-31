9ca0e8c668f9f0d587f92f588c94f6a2f426983e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-07-31 22:04:22 +0300

    Update AxeTests.cs

--- CSharpOOPVI2020/09CSharpAdvancedOOPVI2020_UnitTestingLab/01TestAxe.Tests/AxeTests.cs
+++ CSharpOOPVI2020/09CSharpAdvancedOOPVI2020_UnitTestingLab/01TestAxe.Tests/AxeTests.cs
@@ -3,34 +3,34 @@
 [TestFixture]
 public class AxeTests
 {
-    private const int AxeAttackStartPontsCount = 10;
-    private const int AxeDurabilityStartPontsCount = 10;
-    private const int AxeDurabilityPontsCountAfterAttack = 9;
-    private const int BrokenAxeAttackStartPontsCount = 1;
-    private const int BrokenAxeDurabilityStartPontsCount = 1;
-    private const int DummyHealthStartPontsCount = 10;
-    private const int DummyExperienceStartPontsCount = 10;
+    private const int AxeAttackStartPointsCount = 10;
+    private const int AxeDurabilityStartPointsCount = 10;
+    private const int AxeDurabilityPointsCountAfterAttack = 9;
+    private const int BrokenAxeAttackStartPointsCount = 1;
+    private const int BrokenAxeDurabilityStartPointsCount = 1;
+    private const int DummyHealthStartPointsCount = 10;
+    private const int DummyExperienceStartPointsCount = 10;
 
     [Test]
     public void AxeLosesDurabilityAfterAttack()
     {
         //Arrange
-        Axe axe = new Axe(AxeAttackStartPontsCount, AxeDurabilityStartPontsCount);
-        Dummy dummy = new Dummy(DummyHealthStartPontsCount, DummyExperienceStartPontsCount);
+        Axe axe = new Axe(AxeAttackStartPointsCount, AxeDurabilityStartPointsCount);
+        Dummy dummy = new Dummy(DummyHealthStartPointsCount, DummyExperienceStartPointsCount);
 
         //Act
         axe.Attack(dummy);
 
         //Assert
-        Assert.That(axe.DurabilityPoints, Is.EqualTo(AxeDurabilityPontsCountAfterAttack));
+        Assert.That(axe.DurabilityPoints, Is.EqualTo(AxeDurabilityPointsCountAfterAttack));
     }
 
     [Test]
     public void BrokenAxeCantAttack()
     {
         //Arrange
-        Axe axe = new Axe(BrokenAxeAttackStartPontsCount, BrokenAxeDurabilityStartPontsCount);
-        Dummy dummy = new Dummy(DummyHealthStartPontsCount, DummyExperienceStartPontsCount);
+        Axe axe = new Axe(BrokenAxeAttackStartPointsCount, BrokenAxeDurabilityStartPointsCount);
+        Dummy dummy = new Dummy(DummyHealthStartPointsCount, DummyExperienceStartPointsCount);
 
         //Act
         axe.Attack(dummy);
