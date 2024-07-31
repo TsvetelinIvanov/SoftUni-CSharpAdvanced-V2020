34f59200867d3d488b7d356d2ae666934591a7a4
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-07-31 19:14:27 +0300

    Update StartUp.cs

--- CSharpOOPVI2020/08CSharpAdvancedOOPVI2020_ReflectionAndAttributesLab/06CodeTracker/StartUp.cs
+++ CSharpOOPVI2020/08CSharpAdvancedOOPVI2020_ReflectionAndAttributesLab/06CodeTracker/StartUp.cs
@@ -1,4 +1,4 @@
-﻿namespace AuthorProblem//For Judge have to change the name of csproj file in the Zip file to AuthorProblem.csproj
+﻿namespace AuthorProblem//For Judge: you have to change the name of csproj file in the Zip file to AuthorProblem.csproj
 {
     [Author("Ventsi")]
     public class StartUp
