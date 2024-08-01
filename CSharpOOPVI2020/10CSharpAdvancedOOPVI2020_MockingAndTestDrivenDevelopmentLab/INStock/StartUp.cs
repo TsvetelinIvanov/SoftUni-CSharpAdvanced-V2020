3c8a9b1e861d49cc74c62191d34dc6f9d23154b5
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-01 09:06:04 +0300

    Update StartUp.cs

--- CSharpOOPVI2020/10CSharpAdvancedOOPVI2020_MockingAndTestDrivenDevelopmentLab/INStock/StartUp.cs
+++ CSharpOOPVI2020/10CSharpAdvancedOOPVI2020_MockingAndTestDrivenDevelopmentLab/INStock/StartUp.cs
@@ -12,9 +12,11 @@ namespace INStock
             Product product2 = new Product("Product2", 10, 100);
             Product product3 = new Product("Product3", 10, 100);
             ProductStock products = new ProductStock();
+            
             products.Add(product1);
             products.Add(product2);
             products.Add(product3);
+            
             IProduct product = products.FindByLabel("Product1");
             Console.WriteLine(product.Label);
         }
