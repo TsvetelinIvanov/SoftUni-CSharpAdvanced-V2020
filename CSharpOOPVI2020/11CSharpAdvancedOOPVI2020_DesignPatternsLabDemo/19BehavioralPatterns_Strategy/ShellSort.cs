12c5ce152139af8a7e0c3f587a71784b1142915d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-01 17:59:45 +0300

    Update ShellSort.cs

--- CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/19BehavioralPatterns_Strategy/ShellSort.cs
+++ CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/19BehavioralPatterns_Strategy/ShellSort.cs
@@ -7,8 +7,46 @@ namespace _19BehavioralPatterns_Strategy
     {
         public override void Sort(List<string> list)
         {
-            //list.ShellSort(); not-implemented
+            //list.Sort(); ShellSort is not implemented
+            string shellSortString = @"
+    public class ShellSort 
+    {
+        public static void Sort(int[] array) {
+        int n = array.Length;        
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int temp = array[i];
+                int j;
+                for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+                    array[j] = array[j - gap];
+                array[j] = temp;
+            }
+        }
+    }    
+    
+    public static void PrintArray(int[] array) {
+        int n = array.Length;
+        for (int i = 0; i < n; i++)
+            Console.Write(array[i] + " ");
+        Console.WriteLine();
+    }    
+
+    public static void Main(string[] args) {
+        int[] array = { 12, 34, 54, 2, 3 };
+        Console.WriteLine("Array before sorting:");
+        PrintArray(array);
+        Sort(array );
+        Console.WriteLine("Array after sorting:");
+        PrintArray(array);
+    }
+}":
+        
             Console.WriteLine("ShellSorted list ");
+            foreach (string name in list) 
+            {
+                  Console.WriteLine(name);
+            }
         }
     }
 }
