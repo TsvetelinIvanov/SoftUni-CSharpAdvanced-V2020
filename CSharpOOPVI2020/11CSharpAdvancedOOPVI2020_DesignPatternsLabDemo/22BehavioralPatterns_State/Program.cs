6d674cb81a5d54676a6cfbc8adc0f19c9cbe8709
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-01 18:30:52 +0300

    Update Program.cs

--- CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/22BehavioralPatterns_State/Program.cs
+++ CSharpOOPVI2020/11CSharpAdvancedOOPVI2020_DesignPatternsLabDemo/22BehavioralPatterns_State/Program.cs
@@ -10,22 +10,27 @@
             //Deposited 490,00 лв. ---
             //Balance = 490,00 лв.
             //Status = StandardState
+            
             account.Deposit(390.0);
             //Deposited 390,00 лв. ---
             //Balance = 880,00 лв.
             //Status = StandardState
+            
             account.Deposit(540.0);
             //Deposited 540,00 лв. ---
             //Balance = 1 420,00 лв.
             //Status = PremiumState
+            
             account.PayInterest();
             //Interest Paid ---
             //Balance = 1 491,00 лв.
             //Status = PremiumState
+            
             account.Withdraw(2200.0);
             //Withdrew 2 200,00 лв. ---
             //Balance = -709,00 лв.
             //Status = OverdrawnState
+            
             account.Withdraw(1300.0);
             //No funds available for withdrawal!
             //Withdrew 1 300,00 лв. -- -
