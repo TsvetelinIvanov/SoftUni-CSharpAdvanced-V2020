7664900ee91035992b9ee1ffd9935e8fc54b106f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-02 14:00:07 +0300

    Update StartUp.cs

--- CSharpOOPVI2020/ExamCSharpAdvancedOOP15XII2019/Aquariums/StartUp.cs
+++ CSharpOOPVI2020/ExamCSharpAdvancedOOP15XII2019/Aquariums/StartUp.cs
@@ -9,9 +9,11 @@ namespace Aquariums
         {
             Aquarium aquarium = new Aquarium("Aquarium", 10);
             Console.WriteLine(aquarium.Report());
+            
             List<string> list = new List<string>();
             Console.WriteLine("Fish available at {0}: {1}", aquarium.Name, string.Join(", ", list));
             Console.WriteLine("Fish available at {0}: {1}", aquarium.Name, null);
+            
             Aquarium invalidAquarium = new Aquarium("", 10);
         }
     }
