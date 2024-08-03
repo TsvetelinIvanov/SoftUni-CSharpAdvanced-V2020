d3d25de3df38dbc64c48347f8f19960b2bfa8f13
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-03 23:03:53 +0300

    Update Character.cs

--- CSharpOOPVI2020/ExamRetakeCSharpAdvancedOOP19XII2020/01. Structure_WarCroft/Entities/Characters/Character.cs
+++ CSharpOOPVI2020/ExamRetakeCSharpAdvancedOOP19XII2020/01. Structure_WarCroft/Entities/Characters/Character.cs
@@ -10,122 +10,121 @@ namespace WarCroft.Entities.Characters
 {
     public abstract class Character : ICharacter
     {
-		private string name;
-		private double health;
-		private double armor;
+	private string name;
+	private double health;
+	private double armor;
 
-		protected Character(string name, double health, double armor, double abilityPoints, Bag bag)
+	protected Character(string name, double health, double armor, double abilityPoints, Bag bag)
         {
-			this.Name = name;
-			this.BaseHealth = health;
-			this.Health = health;
-			this.BaseArmor = armor;
-			this.Armor = armor;
-			this.AbilityPoints = abilityPoints;
-			this.Bag = bag;
+	     this.Name = name;
+	     this.BaseHealth = health;
+	     this.Health = health;
+	     this.BaseArmor = armor;
+	     this.Armor = armor;
+	     this.AbilityPoints = abilityPoints;
+	     this.Bag = bag;
         }
 
-		public string Name
+	public string Name
         {
-			get { return this.name; }
+	    get { return this.name; }
             set
             {
-				if (string.IsNullOrWhiteSpace(value))
+		if (string.IsNullOrWhiteSpace(value))
                 {
-					throw new ArgumentException(ExceptionMessages.CharacterNameInvalid);
-				}
-
-				this.name = value;
-            }
-        }
-
-		public double BaseHealth { get; }
-		
-
-		public double Health
-        {
-			get { return this.health; }
-            set
-            {
-				if (value < 0)
-                {
-					value = 0;
-                }
-
-				if (value > this.BaseHealth)
-                {
-					value = this.BaseHealth;
-                }
-
-				this.health = value;
-            }
-        }
-
-		public double BaseArmor { get; }
-
-		public double Armor
-		{
-			get { return this.armor; }
-			set
-			{
-				if (value < 0)
-				{
-					value = 0;
-				}
-
-				this.armor = value;
-			}
+		     throw new ArgumentException(ExceptionMessages.CharacterNameInvalid);
 		}
 
-		public double AbilityPoints { get; }
-
-		public Bag Bag { get; }
-
-		public bool IsAlive { get; set; } = true;		
-
-		public void TakeDamage(double hitPoints)
-        {
-			this.EnsureAlive();
-
-			if (this.Armor >= hitPoints)
-            {
-				this.Armor -= hitPoints;
-				hitPoints = 0;
+		this.name = value;
             }
-            else
-            {
-				hitPoints -= this.Armor;
-				this.Armor = 0;
-            }
-
-			if (this.Health >= hitPoints)
-            {
-				this.Health -= hitPoints;
-            }
-            else
-            {
-				this.Health = 0;				
-            }
-
-			if (this.Health == 0)
-            {
-				this.IsAlive = false;
-			}
         }
 
-		public void UseItem(Item item)
-        {
-			this.EnsureAlive();
+	public double BaseHealth { get; }		
 
-			item.AffectCharacter(this);
+	public double Health
+        {
+	    get { return this.health; }
+            set
+            {
+		if (value < 0)
+                {
+		     value = 0;
+                }
+
+		if (value > this.BaseHealth)
+                {
+		     value = this.BaseHealth;
+                }
+
+		this.health = value;
+            }
         }
 
-		protected void EnsureAlive()
-		{
-			if (!this.IsAlive)
-			{
-				throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
-			}
-		}		
+	public double BaseArmor { get; }
+
+	public double Armor
+	{
+	     get { return this.armor; }
+	     set
+	     {
+		  if (value < 0)
+		  {
+		      value = 0;
+		  }
+
+		   this.armor = value;
+	      }
 	}
+
+	public double AbilityPoints { get; }
+
+	public Bag Bag { get; }
+
+	public bool IsAlive { get; set; } = true;		
+
+	public void TakeDamage(double hitPoints)
+        {
+	    this.EnsureAlive();
+
+	    if (this.Armor >= hitPoints)
+            {
+		this.Armor -= hitPoints;
+		hitPoints = 0;
+            }
+            else
+            {
+		hitPoints -= this.Armor;
+		this.Armor = 0;
+            }
+
+	    if (this.Health >= hitPoints)
+            {
+		this.Health -= hitPoints;
+            }
+            else
+            {
+		this.Health = 0;				
+            }
+
+	    if (this.Health == 0)
+            {
+		this.IsAlive = false;
+	    }
+        }
+
+	public void UseItem(Item item)
+        {
+	     this.EnsureAlive();
+
+	     item.AffectCharacter(this);
+        }
+
+	protected void EnsureAlive()
+	{
+	      if (!this.IsAlive)
+	      {
+		  throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
+	      }
+        }		
+    }
 }
