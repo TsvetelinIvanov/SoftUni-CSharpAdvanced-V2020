50374534f00ae960026c4764db275f53e2180013
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-04 00:46:11 +0300

    Update StartUp.cs

--- CSharpOOPVI2020/ExamRetakeCSharpAdvancedOOP19XII2020/03. Unit Tests_FestivalManager/FestivalManager/StartUp.cs
+++ CSharpOOPVI2020/ExamRetakeCSharpAdvancedOOP19XII2020/03. Unit Tests_FestivalManager/FestivalManager/StartUp.cs
@@ -8,12 +8,12 @@
 		public static void Main(string[] args)
 		{
 			//example usage of the Stage class
-			var song1 = new Song("Ветрове", new TimeSpan(0,3,30));
-			var song2 = new Song("Бурни Нощи", new TimeSpan(0, 2, 30));
-			var song3 = new Song("Ветрове и Бурни Нощи", new TimeSpan(0, 2, 30));
+			Song song1 = new Song("Ветрове", new TimeSpan(0,3,30));
+			Song song2 = new Song("Бурни Нощи", new TimeSpan(0, 2, 30));
+			Song song3 = new Song("Ветрове и Бурни Нощи", new TimeSpan(0, 2, 30));
 
-			var performer1 = new Performer("Ivan", "Ivanov", 19);
-			var performer2 = new Performer("Ivanko", "Ivanov", 19);
+			Performer performer1 = new Performer("Ivan", "Ivanov", 19);
+			Performer performer2 = new Performer("Ivanko", "Ivanov", 19);
 			Stage stage = new Stage();
 
 			stage.AddSong(song1);
