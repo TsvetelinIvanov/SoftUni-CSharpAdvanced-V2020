c5d754234cbca0f06f7c3d35ac57b324eaf9fede
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-03 23:16:58 +0300

    Update Item.cs

--- CSharpOOPVI2020/ExamRetakeCSharpAdvancedOOP19XII2020/01. Structure_WarCroft/Entities/Items/Item.cs
+++ CSharpOOPVI2020/ExamRetakeCSharpAdvancedOOP19XII2020/01. Structure_WarCroft/Entities/Items/Item.cs
@@ -7,19 +7,19 @@ namespace WarCroft.Entities.Items
 	// Christmas came early this year - this class is already implemented for you!
 	public abstract class Item
 	{
-		protected Item(int weight)
-		{
-			this.Weight = weight;
-		}
+	     protected Item(int weight)
+	     {
+		this.Weight = weight;
+	     }
 
-		public int Weight { get; }
+	      public int Weight { get; }
 
-		public virtual void AffectCharacter(Character character)
+	      public virtual void AffectCharacter(Character character)
+	      {
+		if (!character.IsAlive)
 		{
-			if (!character.IsAlive)
-			{
-				throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
-			}			
-		}
+		     throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
+		}			
+	      } 
 	}
 }
