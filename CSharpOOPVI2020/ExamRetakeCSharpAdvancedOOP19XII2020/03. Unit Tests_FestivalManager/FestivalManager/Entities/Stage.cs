294aba3e4683d3303c2b35ad9b4415a7f094becd
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-08-04 00:43:00 +0300

    Update Stage.cs

--- CSharpOOPVI2020/ExamRetakeCSharpAdvancedOOP19XII2020/03. Unit Tests_FestivalManager/FestivalManager/Entities/Stage.cs
+++ CSharpOOPVI2020/ExamRetakeCSharpAdvancedOOP19XII2020/03. Unit Tests_FestivalManager/FestivalManager/Entities/Stage.cs
@@ -8,12 +8,12 @@
     {
         private const string CanNotBeNullMessage = "Can not be null!";
 
-        private readonly List<Song> Songs;
+        private readonly List<Song> songs;
 	private readonly List<Performer> performers;
 
         public Stage()
         {
-            this.Songs = new List<Song>();
+            this.songs = new List<Song>();
             this.performers = new List<Performer>();
         }
 
@@ -40,7 +40,7 @@
                 throw new ArgumentException("You can only add songs that are longer than 1 minute.");
             }
 
-            this.Songs.Add(song);
+            this.songs.Add(song);
         }
 
         public string AddSongToPerformer(string songName, string performerName)
@@ -48,8 +48,8 @@
             this.ValidateNullValue(songName, nameof(songName), CanNotBeNullMessage);
             this.ValidateNullValue(performerName, nameof(performerName), CanNotBeNullMessage);
 
-            var perfomer = this.GetPerformer(performerName);
-            var song = this.GetSong(songName);
+            Performer perfomer = this.GetPerformer(performerName);
+            Song song = this.GetSong(songName);
 
             perfomer.SongList.Add(song);
 
@@ -58,15 +58,14 @@
 
         public string Play()
         {
-            var songsCount = this.performers.Sum(p => p.SongList.Count());
+            int songsCount = this.performers.Sum(p => p.SongList.Count());
 
             return $"{this.performers.Count} performers played {songsCount} songs";
         }
 
         private Performer GetPerformer(string performerName)
         {
-            var performer = this.Performers.FirstOrDefault(p => p.FullName == performerName);
-
+            Performer performer = this.Performers.FirstOrDefault(p => p.FullName == performerName);
             if (performer == null)
             {
                 throw new ArgumentException("There is no performer with this name.");
@@ -77,9 +76,8 @@
 
         private Song GetSong(string songName)
         {
-            var song = this.Songs.FirstOrDefault(p => p.Name == songName);
-
-            if (song==null)
+            Song song = this.Songs.FirstOrDefault(p => p.Name == songName);
+            if (song == null)
             {
                 throw new ArgumentException("There is no song with this name.");
             }
